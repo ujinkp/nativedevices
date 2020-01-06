23f9e43221b330d83ff593291ddb7567309db1fb
Ujin <valikkp@gmail.com>
2020-01-06 22:25:57 +0200

    235. Storing Data in the Local Database

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,22 +1,34 @@
 import * as FileSystem from 'expo-file-system';
 
 export const ADD_PLACE = 'ADD_PLACE';
+import { insertPlace } from '../helpers/db';
 
 export const addPlace = (title, image) => {
-    return async dispatch => {
-        const fileName = image.split('/').pop();
-        const newPath = FileSystem.documentDirectory + fileName;
+	return async dispatch => {
+		const fileName = image.split('/').pop();
+		const newPath = FileSystem.documentDirectory + fileName;
 
-        try {
-            await FileSystem.moveAsync({
-                from: image,
-                to: newPath
-            });
-        } catch (err) {
-            console.log(err);
-            throw err; 
-        }
+		try {
+			await FileSystem.moveAsync({
+				from: image,
+				to: newPath
+			});
+			const dbResult = await insertPlace(
+				title,
+				newPath,
+				'Dummy address',
+				15.6,
+				12.3
+			);
+			console.log(dbResult);
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
 
-        dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath }});
-    };
+		dispatch({
+			type: ADD_PLACE,
+			placeData: { id: dbResult.insertId, title: title, image: newPath }
+		});
+	};
 };
