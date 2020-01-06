4b05c1b354c0b30fbcba69cba675ea8cf643af73
Ujin <valikkp@gmail.com>
2020-01-06 12:14:06 +0200

    227. Getting Started with the Form

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,18 +1,61 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet
+} from "react-native";
+import Colors from "../constants/Colors";
 
- const NewPlaceScreen = props => {
-    return (
-        <View>
-          <Text>NewPlaceScreen</Text>
-        </View>
-      );
- };
+const NewPlaceScreen = props => {
+  const [titleValue, setTitleValue] = useState("");
 
- NewPlaceScreen.navigationOptions = {
-   headerTitle: 'Add Place'
- };
+  const titleChangeHandler = text => {
+    setTitleValue(text);
+  };
 
- const styles = StyleSheet.create({});
+  const savePlaceHandler = () => {};
 
- export default NewPlaceScreen;
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          title={titleValue}
+        />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+NewPlaceScreen.navigationOptions = {
+  headerTitle: "Add Place"
+};
+
+const styles = StyleSheet.create({
+  from: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    marginVertical: 4,
+    marginHorizontal: 2
+  }
+});
+
+export default NewPlaceScreen;
