844d4215cf8a8ff0823144291e2024cc57f44c37
Ujin <valikkp@gmail.com>
2020-01-13 21:26:27 +0200

    241. Adding a Marker

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
- const MapScreen = props => {
-  const mapRegion = {
-    latitude: 37.78,
-    longitude: -122.43,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
-  };
-  
-  return (
-        <MapView style={styles.map} region={mapRegion}/>
-      );
- };
+const MapScreen = props => {
+	const [selectedLocation, setSelectedLocation] = useState();
 
- const styles = StyleSheet.create({
-   map: {
-     flex: 1
-   }
- });
+	const mapRegion = {
+		latitude: 37.78,
+		longitude: -122.43,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421
+	};
 
- export default MapScreen;
+	const selectLocationHandler = event => {
+		setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude,
+    });
+	};
+
+	let markerCoordinates;
+
+	if (selectedLocation) {
+		markerCoordinates = {
+			latitude: selectedLocation.lat,
+			longitude: selectedLocation.lng
+		};
+	}
+
+	return (
+		<MapView
+			style={styles.map}
+			region={mapRegion}
+			onPress={selectLocationHandler}
+		>
+			{markerCoordinates && (
+				<Marker
+					title="Picked Location"
+					coordinate={markerCoordinates}
+				></Marker>
+			)}
+		</MapView>
+	);
+};
+
+const styles = StyleSheet.create({
+	map: {
+		flex: 1
+	}
+});
+
+export default MapScreen;
