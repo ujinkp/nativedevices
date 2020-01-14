f1e1db3edcce7e64c3b3dcede28f16471ceecc9c
Ujin <valikkp@gmail.com>
2020-01-14 19:20:28 +0200

    244. Updating the Location Screen When the Location Changes

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -19,11 +19,14 @@ const LocationPicker = props => {
 
 	const mapPickedLocation = props.navigation.getParam('pickedLocation');
 
+	const {onLocationPicked} = props;
+
 	useEffect(() => {
 		if (mapPickedLocation) {
 			setPickedLocation (mapPickedLocation)
+			onLocationPicked(mapPickedLocation);
 		}
-	}, [mapPickedLocation]);
+	}, [mapPickedLocation, onLocationPicked]);
 
 	const verifyPermissions = async () => {
 		const result = await Permissions.askAsync(Permissions.LOCATION);
@@ -51,6 +54,10 @@ const LocationPicker = props => {
 			});
 			console.log(location);
 			setPickedLocation({
+				lat: location.coords.latitude,
+				lng: location.coords.longitude
+			});
+			props.onLocationPicked({
 				lat: location.coords.latitude,
 				lng: location.coords.longitude
 			});
