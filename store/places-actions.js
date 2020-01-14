d7f937a67700035f950b1bf93de7512533190d75
Ujin <valikkp@gmail.com>
2020-01-14 19:54:43 +0200

    245. Storing the Address

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,13 +1,27 @@
 import * as FileSystem from 'expo-file-system';
 
 import { insertPlace, fetchPlaces } from '../helpers/db';
+import ENV from '../env';
 
 export const ADD_PLACE = 'ADD_PLACE';
 export const SET_PLACES = 'SET_PLACES';
 
-
-export const addPlace = (title, image) => {
+export const addPlace = (title, image, location) => {
 	return async dispatch => {
+		const response = fetch(
+			`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${ENV.googleApiKey}`
+		);
+
+		if (!response.ok) {
+			throw new Error('Something went wrong');
+		}
+
+		const resData = await response.json();
+		if (!resData.result) {
+			throw new Error('Something went wrong');
+		}
+
+		const address = resData.results[0].formatted_address;
 		const fileName = image.split('/').pop();
 		const newPath = FileSystem.documentDirectory + fileName;
 
@@ -19,9 +33,9 @@ export const addPlace = (title, image) => {
 			const dbResult = await insertPlace(
 				title,
 				newPath,
-				'Dummy address',
-				15.6,
-				12.3
+				address,
+				location.lat,
+				location.lng
 			);
 			console.log(dbResult);
 		} catch (err) {
@@ -31,19 +45,25 @@ export const addPlace = (title, image) => {
 
 		dispatch({
 			type: ADD_PLACE,
-			placeData: { id: dbResult.insertId, title: title, image: newPath }
+			placeData: {
+				id: dbResult.insertId,
+				title: title,
+				image: newPath,
+				address: address,
+				coords: { lat: location.lat, lng: location.lng }
+			}
 		});
 	};
 };
 
 export const loadPlaces = () => {
-	 return async dispatch => {
-		 try {
-			 const dbResult = await fetchPlaces(); 
-			 console.log(dbResult);
-			 dispatch({ type: SET_PLACES, places: dbResult.rows._array });
-		 } catch (err) {
+	return async dispatch => {
+		try {
+			const dbResult = await fetchPlaces();
+			console.log(dbResult);
+			dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+		} catch (err) {
 			throw err;
-		 }
-	 };
-}; 
+		}
+	};
+};
--- store/places-reducer.js
+++ store/places-reducer.js
@@ -9,13 +9,26 @@ export default (state = initialState, action) => {
 	switch (action.type) {
 		case SET_PLACES:
 			return {
-				places: action.places.map(pl => new Place(pl.id.toString(), pl.title, pl.imageUri))
+				places: action.places.map(
+					pl =>
+						new Place(
+							pl.id.toString(),
+							pl.title,
+							pl.imageUri,
+							pl.address,
+							pl.lat,
+							pl.lng
+						)
+				)
 			};
 		case ADD_PLACE:
 			const newPlace = new Place(
 				action.placeData.id.toString(),
 				action.placeData.title,
-				action.placeData.image
+				action.placeData.image,
+				action.placeData.address,
+				action.placeData.coords.lat,
+				action.placeData.coords.lng
 			);
 			return {
 				places: state.places.concat(newPlace)
